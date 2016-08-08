e7fe9961d5985931ad9cf75685577f71fca5fe80
Xu Shen <shenxuustc@gmail.com>
2016-08-08 16:01:24 +0800

    refine video_unroll_layer

--- include/caffe/layers/video_unroll_layer.hpp
+++ include/caffe/layers/video_unroll_layer.hpp
@@ -29,6 +29,7 @@ namespace caffe{
 			const vector<Blob<Dtype>*>& top){}
 		virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom, 
 			const vector<Blob<Dtype>*>& top){}
+
 		virtual void Backward_cpu(const vector<Blob<Dtype>*>& top, 
 			const vector<bool>& propagate_down, 
 			const vector<Blob<Dtype>*>& bottom){
